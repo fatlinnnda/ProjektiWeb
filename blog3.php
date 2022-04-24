3e6590c92f0e7e59dde103162eee00037c59018f
Fatlinda0 <fb55487@ubt-uni.net>
2022-04-24 16:00:15 +0200

    blog3

---
+++ blog3.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>H A L T therapy - Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include 'nav.php';  ?>
+    <div id="slider">
+        <input type="radio" name="slider" id="slide1" checked>
+        <input type="radio" name="slider" id="slide2">
+        <input type="radio" name="slider" id="slide3">
+        <div id="slides">
+            <div id="overflow">
+               <div class="inner">
+                  <div class="slide slide_1">
+                     <div class="slide-content">
+                         <img src="img/motivational-text-yellow-background.jpg" alt="" class="Mental-HealthCare">
+                     </div>
+                  </div>
+                  <div class="slide slide_2">
+                     <div class="slide-content">
+                         <img src="img/think-outside-box-quotes-business-concept.jpg" alt="" class="Mental-HealthCare"> 
+                     </div>
+                  </div>
+                  <div class="slide slide_3">
+                     <div class="slide-content">
+                         <img src="img/fab-lentz-mRMQwK513hY-unsplash.jpg" alt="" class="Mental-HealthCare">
+                     </div>
+                  </div>
+                  
+               </div>
+            </div>
+         </div>
+         <div id="controls">
+            <label for="slide1"></label>
+            <label for="slide2"></label>
+            <label for="slide3"></label>
+           
+         </div>
+         <div id="bullets">
+            <label for="slide1"></label>
+            <label for="slide2"></label>
+            <label for="slide3"></label>
+            
+        </div>
+    </div>
+    <h1 class="h1-blog2">Anxiety Exercises to Help You Relax</h1>
+    <hr id="hr1-2">
+    <br class="br-blog">
+    <div class="p-blog">
+        <p>Anxiety is a typical human reaction to stress. But too much anxiety can get in the way of living a healthy, happy life. 
+            If you feel caught up in your anxiety, try one or a few of the following exercises anytime and anywhere to find relief. 
+            The goal is to perform exercises that can quickly help you relax.</p>
+            <p>Deep breathing can be done pretty much anywhere and anytime you feel anxious, without any special tools or time frame. 
+                Sometimes just taking a few deep breaths before entering a stressful situation or when you find yourself in the middle of one, can lower stress and anxiety levels. 
+                However, if you want to have a more structured time of relaxation and anxiety relief, here are a few exercises you can try.</p>
+                <br>
+            <ol>
+                <li class="li-ol">Relaxing deep breathing
+                    <ul class="ul-blog">
+                        <li>Sit comfortably.</li>
+                        <li>Breathe in through your nose for 6 seconds (try to fill your abdomen first, then up through your upper chest).</li>
+                        <li>Hold your breath for 2-3 seconds.</li>
+                        <li>Release your breath slowly through pursed lips.</li>
+                        <li>Repeat 10 times.</li>
+                        <li>Sit comfortably.</li>
+                        <li>Close your eyes.</li>
+                        <li>Take one deep breath and release it while saying the word “relax” either silently or aloud.</li>
+                        <li>Breathe naturally 10 times while counting each breath (try to relax your face muscles as well as shoulders and other areas).</li>
+                        <li>After counting down from 10 to 1, open your eyes.</li>
+                    </ul>
+                </li>
+                <li class="li-ol">Meditation breathing
+                    <ul class="ul-blog">
+                        <li>Breathe out slowly.</li>
+                        <li>Ignore when you breathe in.</li>
+                        <li>Concentrate only on breathing out.</li>
+                        <li>Breathe out as much air as possible before breathing in again.</li>
+                        <li>Focus on relaxing the muscles in your face, shoulders, and anywhere else you feel tense while you breathe.</li>
+                    </ul>
+                </li>
+            </ol><br>
+            <h3>Related: 
+                <a href="https://declutterthemind.com/blog/anxiety-exercises/">
+                    Anxiety Exercises: 15 Things You Can Do to Lower Anxiety
+                </a>
+            </h3>
+        </div>
+        <?php include 'footer.php';  ?>
+    <script src="js/main.js"></script>
+</body>
+</html>
