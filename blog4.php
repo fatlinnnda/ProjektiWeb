f7431a4b30e1ba16726f0efa12dc9f254ff51800
fatlinnnda <104312337+fatlinnnda@users.noreply.github.com>
2022-04-24 16:43:17 +0200

    Create blog4.php

---
+++ blog4.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>H A L T therapy - Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php include 'nav.php';  ?>
+    <div id="slider">
+        <input type="radio" name="slider" id="slide1" checked>
+        <input type="radio" name="slider" id="slide2">
+        <input type="radio" name="slider" id="slide3">
+        <div id="slides">
+            <div id="overflow">
+               <div class="inner">
+                  <div class="slide slide_1">
+                     <div class="slide-content">
+                         <img src="img/motivational-text-yellow-background.jpg" alt="" class="Mental-HealthCare">
+                     </div>
+                  </div>
+                  <div class="slide slide_2">
+                     <div class="slide-content">
+                         <img src="img/think-outside-box-quotes-business-concept.jpg" alt="" class="Mental-HealthCare"> 
+                     </div>
+                  </div>
+                  <div class="slide slide_3">
+                     <div class="slide-content">
+                         <img src="img/fab-lentz-mRMQwK513hY-unsplash.jpg" alt="" class="Mental-HealthCare">
+                     </div>
+                  </div>
+                  
+               </div>
+            </div>
+         </div>
+         <div id="controls">
+            <label for="slide1"></label>
+            <label for="slide2"></label>
+            <label for="slide3"></label>
+           
+         </div>
+         <div id="bullets">
+            <label for="slide1"></label>
+            <label for="slide2"></label>
+            <label for="slide3"></label>
+            
+        </div>
+    </div>
+    <h1 class="h1-blog">Morning Routine</h1>
+    <hr id="hr1">
+    <br class="br-blog">
+    <div class="p-blog">
+        <p>Why do you need a daily morning routine for success?</p><br>
+        <p>What you do in the morning – good or bad – sets the tone for the day. And your habits – good or bad – determine the quality of your life.</p><br>
+        <p>There’s no such thing as a “perfect morning routine for success.” However, there is a productive morning routine that’s best for you.
+        So, if you want to learn how to create a morning routine that’s perfect for you, try following these five simple steps.</p>
+        <br><h2>Find Your “Why”</h2><br>
+        <p>At first, sticking to a daily morning routine is tough. So, you need to know why you’re doing it. <br>
+            For example, perhaps you want to earn more money, feel empowered, start a side hustle, write a book, get fit, eat healthier, or learn a new skill. Or perhaps you want to improve your mood, concentration, or sense of well being.
+            Then, ask yourself why you want these things. <br>
+            Maybe you want to earn more money so you can travel to Thailand, buy a nicer house for your family, or retire early? <br>
+            Whatever it is, you need to have some good reasons to get up at 5 am.</p>
+            <p>Remember:Your “why” is what will keep you going when times get tough – and they will get tough before they get easier.</p>
+            <br><h2>Start Small</h2><br>
+            <p>When developing new habits, it’s always best to take small, consistent steps over time.James Clear, the author of Atomic Habits, wrote, “Changes that seem small and unimportant at first will compound into remarkable results if you’re willing to stick with them for years.”
+                Don’t shock your system. Change gradually, but consistently. Try to find one or two things to start with – three max. And start small. After a week of consistent wins, increase the challenge a little bit – just a little bit.
+            </p>
+            <h3>Related: 
+                <a href="https://www.throughthephases.com/productive-morning-routine/">
+                    10 Morning Habits You Need in 2022 for Productivity and Success
+                </a>
+            </h3>
+        </div>
+    
+        <footer>
+        <h1>Questions?</h1>
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter"></i>
+        <br><br>
+        <hr id="hr_">
+        <div class="credit">created by <span>Alsona Smajli & Fatlinda Berisha</span> | all rights reserved</div>
+    </footer>
+    <script src="js/main.js"></script>
+</body>
+</html>
