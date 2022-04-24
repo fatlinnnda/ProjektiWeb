7521bd288c0c4a57faf0e08db388ccc4d5fcbe4e
Fatlinda0 <fb55487@ubt-uni.net>
2022-04-24 15:59:48 +0200

    blog2

---
+++ blog2.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>H A L T therapy - Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include 'nav.php';  ?>
+    <div id="slider">
+        <input type="radio" name="slider" id="slide1" checked>
+        <input type="radio" name="slider" id="slide2">
+        <input type="radio" name="slider" id="slide3">
+        <div id="slides">
+            <div id="overflow">
+               <div class="inner">
+                  <div class="slide slide_1">
+                     <div class="slide-content">
+                         <img src="img/motivational-text-yellow-background.jpg" alt="" class="Mental-HealthCare">
+                     </div>
+                  </div>
+                  <div class="slide slide_2">
+                     <div class="slide-content">
+                         <img src="img/think-outside-box-quotes-business-concept.jpg" alt="" class="Mental-HealthCare"> 
+                     </div>
+                  </div>
+                  <div class="slide slide_3">
+                     <div class="slide-content">
+                         <img src="img/fab-lentz-mRMQwK513hY-unsplash.jpg" alt="" class="Mental-HealthCare">
+                     </div>
+                  </div>
+                  
+               </div>
+            </div>
+         </div>
+         <div id="controls">
+            <label for="slide1"></label>
+            <label for="slide2"></label>
+            <label for="slide3"></label>
+           
+         </div>
+         <div id="bullets">
+            <label for="slide1"></label>
+            <label for="slide2"></label>
+            <label for="slide3"></label>
+            
+        </div>
+    </div>
+    <h1 class="h1-blog">Mental Health Day Off</h1>
+    <hr id="hr1">
+    <br class="br-blog">
+    <div class="p-blog">
+        <p>Who among us hasn't needed a break from work? No matter how much you may love your job, sometimes you have to get away from it. You might put in for a vacation day or a personal day, 
+        depending on your company's policies and procedures. You might even 
+        <a href="https://money.usnews.com/money/careers/company-culture/articles/how-to-call-in-sick">call in sick</a>. 
+        What you are really taking, though, is a mental health day off.</p>
+        <p>Self-care is anything you do to take care of yourself so you can stay physically, mentally, and emotionally well. 
+            Its benefits are better physical, mental, and emotional health and well-being. Research suggests self-care promotes positive health 
+            outcomes, such as fostering resilience, living longer, and becoming better equipped to manage stress.</p>
+        <br><br>
+        <h2>Take a Day</h2>
+        <br>
+        <p>Once you have decided to take a mental health day, the question then becomes "when and how?"</p><br>
+        <p>The best way to avoid feeling stressed about taking a day off is to schedule a day off ahead of time, 
+            ensuring that you've taken steps to rearrange your workload or find a replacement for the day. 
+            However, if you wake up in the morning and just feel that you can't face the stress of the day, 
+            this may be a good time to take a mental health day and make the most of it.</p>
+            <p>The decision of when to take a mental health day really depends on your personal situation. 
+                If your employer is supportive and you are comfortable sharing your reasons, feel free to plan a mental health day out in advance. 
+                If you don't want to divulge your reasons for taking a day or if your employer is less understanding, don't feel obligated to share.</p> 
+            <br>
+            <p>You can:</p> <br>
+            <ul class="ul-blog">
+                <li>Call in sick</li>
+                <li>Plan to use some of your time off</li> 
+                <li>Schedule your mental health day for a day you already have off</li>
+            </ul><br>
+            <h3>Related: 
+                <a href="https://www.everydayhealth.com/self-care/start-a-self-care-routine/#:~:text=A%205-Step%20Approach%20for%20Creating%20%28and%20Getting%20Into%29,and%20tweak%20your%20approach%20as%20you%20go.%20">
+                    How to Start a Self-Care Routine You’ll Follow
+                </a>
+            </h3>
+        </div>
+    
+        <?php include 'footer.php';  ?>
+    <script src="js/main.js"></script>
+</body>
+</html>
